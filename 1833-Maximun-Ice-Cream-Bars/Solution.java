777c95baf87b210211fa4372100975880128d112
zzkevin <realzzkevin@gmail.com>
2023-01-06 18:25:54 -0500

    solution, how can this problem be medium?

--- 1833-Maximun-Ice-Cream-Bars/Solution.java
+++ 1833-Maximun-Ice-Cream-Bars/Solution.java
@@ -1,3 +1,17 @@
 public class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int total = 0;
+        int i = 0;
 
+        while (coins > 0 && i < costs.length){
+            if(coins >= costs[i]){
+                coins -= costs[i];
+                total++;
+            }
+            i++;
+        }
+
+        return total;
+    }
 }
