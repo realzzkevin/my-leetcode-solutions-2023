88333084481005ade7eebc68a51863b9597676c8
zzkevin <realzzkevin@gmail.com>
2023-01-07 13:27:39 -0500

    add two bitwise solution for 191

--- 191-Number-Of-1-Bits/Solution.java
+++ 191-Number-Of-1-Bits/Solution.java
@@ -9,4 +9,23 @@ public class Solution {
         }
         return sum;
     }
+
+    //bitwise solution 1
+    // int count = 0;
+    // int mask = 1;
+    // for (int i = 0; i < 32; i++) {
+    //     if((n & mask) != 0) {
+    //         count++;
+    //     }
+    //     mask <<= 1;
+    // }
+    // return count;
+
+    //bitwise solutuion 2
+    // int count = 0;
+    // while (n != 0) {
+    //     count++;
+    //     n &= (n-1);
+    // }
+    // return count;
 }
