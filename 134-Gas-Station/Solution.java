a5731d9e8add58b2df458cc2f6a721f32fbe03a0
zzkevin <realzzkevin@gmail.com>
2023-01-07 08:59:16 -0500

    two pass, too slow

--- 134-Gas-Station/Solution.java
+++ 134-Gas-Station/Solution.java
@@ -1,3 +1,26 @@
 public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int[] diff = new int[gas.length];
+        int sum = 0;
+        for(int i = 0; i < gas.length; i++) {
+            diff[i] = gas[i] - cost[i];
+            sum += diff[i];
+        }
 
+        if(sum >= 0) {
+            for(int i = 0; i < diff.length; i++) {
+                int next = 0;
+                for(int r = i; r < diff.length; r++) {
+                    next += diff[r];
+                    if(next < 0) {
+                        break;
+                    }
+                }
+                if(next >= 0) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
 }
