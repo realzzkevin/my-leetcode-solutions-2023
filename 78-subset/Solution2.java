bc80715078bd5a64ec566208621b7de7cff98bd6
zzkevin <realzzkevin@gmail.com>
2023-01-08 23:42:08 -0500

    add mask solution

--- 78-subset/Solution2.java
+++ 78-subset/Solution2.java
@@ -1,15 +1,16 @@
-class Solution2 {
+class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>  answer = new ArrayList<>();
-        answer.add(new ArrayList<Integer>());
-        for(int i = 0; i < nums.length; i++) {
-            int n = answer.size();
-            for(int j = 0; j < n; j++) {
-                List<Integer> temp = new ArrayList<>();
-                temp.addAll(answer.get(j));
-                temp.add(nums[i]);
-                answer.add(temp);
+        int n = nums.length;
+        for (int i = (int)Math.pow(2, n); i < Math.pow(2, n+1); i++){
+            String mask = Integer.toBinaryString(i).substring(1);
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j <  n; j++ ) {
+                if(mask.charAt(j) == '1'){
+                    list.add(nums[j]);
+                }
             }
+            answer.add(list);
         }
         return answer;
     }
