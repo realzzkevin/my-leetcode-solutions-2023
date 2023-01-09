4447347c796904d2df89a9a60a4b8b0a949c3bb1
zzkevin <realzzkevin@gmail.com>
2023-01-08 21:17:15 -0500

    Iterative solution

--- 144-Binary-Tree-Preorder-Traversal/Solution.java
+++ 144-Binary-Tree-Preorder-Traversal/Solution.java
@@ -19,4 +19,23 @@ class Solution {
         nextNode(tree, node.left);
         nextNode(tree, node.right);
     }
+
+    /*
+        public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> nodeStack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        nodeStack.push(root);
+        while(!nodeStack.isEmpty()){
+            TreeNode node = nodeStack.pop();
+            if(node != null) {
+                list.add(node.val);
+                nodeStack.push(node.right);
+                nodeStack.push(node.left);
+            }
+
+        }
+
+        return list;
+
+     */
 }
