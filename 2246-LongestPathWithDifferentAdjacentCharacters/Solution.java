9ff530fd1e0c28095d6f2dfe4d70bdef0e112418
zzkevin <realzzkevin@gmail.com>
2023-01-13 15:23:58 -0500

    try to give a DFS solution

---
+++ 2246-LongestPathWithDifferentAdjacentCharacters/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPath(int[] parent, String s) {
+
+    }
+
+    public int Dfs(int index, int parent, String s) {
+        int root = 0;
+        int x = index;
+        int y = 0;
+        String
+        //first half start from the node;
+        while(parent[y] != -1) {
+            parent y
+        }
+
+        //second half start from the root;
+    }
+}
