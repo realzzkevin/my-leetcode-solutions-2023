9fc0c8f528c04643c2b679b5d858c9b2c80c8079
zzkevin <realzzkevin@gmail.com>
2023-01-17 16:32:36 -0500

    first try

---
+++ 926-FlipStringToMonotoneIncreasing/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int leftFlips = 0;
+        int rightFlips = 0;
+        int minFlips = 0;
+        int n = s.length();
+
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) == '0'){
+                rightFlips++;
+            }
+        }
+
+        minFlips = rightFlips;
+
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) == '1'){
+                leftFlips++;
+            } else if(s.charAt(i) =='0'){
+                rightFlips--;
+            }
+
+            minFlips = Math.min(minFlips, leftFlips + rightFlips);
+            if(minFlips == 0){
+                return minFlips;
+            }
+        }
+
+        return minFlips;
+    }
+}
