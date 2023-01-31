c6512c5a4c7ab9747b6fab31b8d93f73a9115c66
realzzkevin <realzzkevin@gmail.com>
2023-01-31 01:20:09 -0500

    problem 1626

--- 1908-GameOfNim/Solution.java
+++ 1908-GameOfNim/Solution.java
@@ -1,4 +1,10 @@
 public class Solution {
-
+    public boolean nimGame(int[] piles) {
+        int nimSum = 0;
+        for(int p : piles) {
+            nimSum ^= p;
+        }
+        return nimSum != 0;
+    }
 }
 
