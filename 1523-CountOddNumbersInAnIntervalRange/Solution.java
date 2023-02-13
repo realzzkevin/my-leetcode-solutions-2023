d422c2a77b0a9dddb4c7ca14c5fe9ed3d62845a2
zzkevin <realzzkevin@gmail.com>
2023-02-12 22:53:15 -0500

    a better solutuion

--- 1523-CountOddNumbersInAnIntervalRange/Solution.java
+++ 1523-CountOddNumbersInAnIntervalRange/Solution.java
@@ -1,14 +1,9 @@
 class Solution {
     public int countOdds(int low, int high) {
-        int count = 0;
-        if(low%2 != 0) {
-            count++;
-            low++;
+        if(low % 2 != 0 || high % 2 != 0) {
+            return ((high - low) / 2) + 1;
+        } else {
+            return (high - low) / 2;
         }
-        if(high%2 != 0) {
-            high++;
-        }
-        count += (high - low) / 2;
-        return count;
     }
 }
