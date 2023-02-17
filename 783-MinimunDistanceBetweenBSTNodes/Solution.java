9ca54ac3b6dd3f9a0adfe9bc273f7180bbb8a2a6
realzzkevin <realzzkevin@gmail.com>
2023-02-16 23:18:16 -0500

    inorder travel with list

--- 783-MinimunDistanceBetweenBSTNodes/Solution.java
+++ 783-MinimunDistanceBetweenBSTNodes/Solution.java
@@ -15,6 +15,21 @@
  */
 class Solution {
     public int minDiffInBST(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        travelTree(root, list);
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+            min = Math.min(min, list.get(i) - list.get(i-1));
+        }
+        return min;
+    }
 
+    public void travelTree(TreeNode node, List<Integer> list){
+        if(node != null){
+            travelTree(node.left, list);
+            list.add(node.val);
+            travelTree(node.right, list);
+        }
     }
 }
