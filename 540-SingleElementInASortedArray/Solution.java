de19cb31e8257e03ebac61ac395e42d3b6d546f6
zzkevin <realzzkevin@gmail.com>
2023-02-21 00:11:13 -0500

    better solutin

--- 540-SingleElementInASortedArray/Solution.java
+++ 540-SingleElementInASortedArray/Solution.java
@@ -1,9 +1,18 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = 0;
-        for(int n : nums){
-            ans ^= n;
+        int l = 0;
+        int r = nums.length - 1;
+        int m = 0;
+        while( l < r) {
+            m = (r + l) / 2;
+            m = m % 2 != 0 ? m-1 : m;
+            if(nums[m + 1] == nums[m]){
+                l = m + 2;
+            } else {
+                r = m;
+            }
         }
-        return ans;
+        return nums[l];
+
     }
 }
