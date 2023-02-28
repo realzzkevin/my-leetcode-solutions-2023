7f49c8d1b4fa8184dd7c2fecfec1a8437d2c70f0
zzkevin <realzzkevin@gmail.com>
2023-02-28 01:20:58 -0500

    init problem 427

--- 427-ConstructQuadTree/Solution.java
+++ 427-ConstructQuadTree/Solution.java
@@ -37,3 +37,26 @@ class Node {
     }
 };
 */
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return solve(grid, 0, 0, grid.length);
+    }
+
+    Node solve(int[][] grid, int x, int y, int length){
+        if(length == 1) {
+            return new Node((grid[x][y] == 1), true);
+        } else {
+            Node tf = solve(grid, x, y, length/2);
+            Node tr = solve(grid, x, y + (length/2), length/2);
+            Node bl = solve(grid, x+(length/2), y, length/2);
+            Node br = solve(grid, x+(length/2), y+(length/2), length/2);
+
+            if((tf.isLeaf && tr.isLeaf && bl.isLeaf && br.isLeaf && (tf.val == tr.val && bl.val == br.val && tf.val == bl.val))) {
+                return new Node(tf.val, true);
+            }
+
+            return new Node(false, false, tf, tr, bl, br);
+        }
+    }
+}
