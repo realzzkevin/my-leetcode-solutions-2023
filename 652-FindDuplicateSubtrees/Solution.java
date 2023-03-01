843959911fc14e2ebc29911b147a1c237e8623dc
zzkevin <realzzkevin@gmail.com>
2023-03-01 00:41:05 -0500

    solutio for problem 652

--- 652-FindDuplicateSubtrees/Solution.java
+++ 652-FindDuplicateSubtrees/Solution.java
@@ -14,5 +14,21 @@
  * }
  */
 class Solution {
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        travelTree(root, new HashMap<String, Integer>(), list);
+        return list;
+    }
 
+    public String travelTree (TreeNode node, Map<String, Integer> map, List<TreeNode> list){
+        String str = "";
+        if(node != null) {
+            str = "(" + travelTree(node.left, map, list) + ")" + node.val + "(" + travelTree(node.right, map, list) +")";
+            map.put(str, map.getOrDefault(str, 0) + 1);
+            if(map.get(str)==2){
+                list.add(node);
+            }
+        }
+        return str;
+    }
 }
