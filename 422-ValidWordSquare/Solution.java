3878ae87fe4cc49b9f7052cd45a18e1f1401b94e
zzkevin <realzzkevin@gmail.com>
2023-03-04 00:47:34 -0500

    straight forward solution for 422

--- 422-ValidWordSquare/Solution.java
+++ 422-ValidWordSquare/Solution.java
@@ -1,3 +1,29 @@
 class Solution {
+    public boolean validWordSquare(List<String> words) {
+        if(words.get(0).length() != words.size()){
+            return false;
+        }
+        for(String s : words) {
+            if(words.size() < s.length()){
+                return false;
+            }
+        }
+        for(int i = 0; i < words.size(); i++) {
+            for(int j = i; j < words.size(); j++ ){
+                char temp1 = ' ';
+                char temp2 = ' ';
+                if(j < words.get(i).length()) {
+                    temp1 = words.get(i).charAt(j);
+                }
+                if(i < words.get(j).length()){
+                    temp2 = words.get(j).charAt(i);
+                }
+                if(temp1 != temp2) {
+                    return false;
+                }
+            }
 
+        }
+        return true;
+    }
 }
