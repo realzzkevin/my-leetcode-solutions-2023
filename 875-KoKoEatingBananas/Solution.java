37ac5f5747913209ed66bef5c449a6ea49134067
zzkevin <realzzkevin@gmail.com>
2023-03-08 17:34:06 -0500

    binary search solutio for 875

--- 875-KoKoEatingBananas/Solution.java
+++ 875-KoKoEatingBananas/Solution.java
@@ -1,3 +1,25 @@
 class Solution {
-
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 0;
+        for(int n : piles) {
+            right = Math.max(n, right);
+        }
+        while(left < right) {
+            int mid = (left + right) / 2;
+            int time = 0;
+            for(int n : piles) {
+                time += n / mid;
+                if(n%mid > 0) {
+                    time++;
+                }
+            }
+            if(time <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
