30f77ff19ee71f05e7c8e00397799c5432dc545a
zzkevin <realzzkevin@gmail.com>
2023-03-08 01:55:54 -0500

    add binary search solution

--- 2187-MinimumTimeToCompleteTrips/Solution.java
+++ 2187-MinimumTimeToCompleteTrips/Solution.java
@@ -17,3 +17,34 @@ class Solution {
         return miniBustime;
     }
 }
+
+//Binary search solution
+class Solution {
+    public long minimumTime(int[] time, int totalTrips) {
+        Arrays.sort(time);
+        long right = (long)time[time.length-1] * totalTrips;
+        System.out.println(right);
+        long left = 1;
+
+        while (left < right) {
+            long mid = (left + right) / 2;
+            if(enoughTime(time, mid, totalTrips)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    public boolean enoughTime(int[] time, long setTime, int totalTrips) {
+        long tripCount = 0;
+        for(int i = 0; i < time.length; i++) {
+            if(setTime / time[i] >0){
+                tripCount += setTime / time[i];
+            } else {
+                break;
+            }
+        }
+        return tripCount >= totalTrips;
+    }
+}
