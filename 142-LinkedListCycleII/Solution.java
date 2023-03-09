5423190b47d51de26da1012f92daa9df23ddf369
zzkevin <realzzkevin@gmail.com>
2023-03-09 00:38:11 -0500

    add solution for problem 142

--- 142-LinkedListCycleII/Solution.java
+++ 142-LinkedListCycleII/Solution.java
@@ -1,3 +1,22 @@
 class Solution {
-
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                fast = head;
+                while(fast != slow) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
 }
