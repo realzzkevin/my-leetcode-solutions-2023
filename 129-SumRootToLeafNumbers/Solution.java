0415a48f784557c4a0392e6befc98a461f842176
zzkevin <realzzkevin@gmail.com>
2023-03-13 23:46:51 -0400

    solution for 129

--- 129-SumRootToLeafNumbers/Solution.java
+++ 129-SumRootToLeafNumbers/Solution.java
@@ -1,3 +1,23 @@
 class Solution {
+    public int sumNumbers(TreeNode root) {
+        int sum = sum(root, 0);
+        return sum;
+    }
 
+    public int sum(TreeNode node, int num) {
+        if(node == null) {
+            return 0;
+        } else {
+            num = (num * 10) + node.val;
+            int sums = 0;
+            if(node.left != null){
+                sums += sum(node.left, num);
+            }
+            if(node.right != null) {
+                sums += sum(node.right, num);
+            }
+
+            return sums == 0 ? num : sums;
+        }
+    }
 }
