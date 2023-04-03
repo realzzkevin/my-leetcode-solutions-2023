97a1eb9748a65c8c37ddd14e5e8e57014bf61e4b
zzkevin <realzzkevin@gmail.com>
2023-04-03 17:15:15 -0400

    solution for 881

--- 881_BoatsToSavePeople/Solution.java
+++ 881_BoatsToSavePeople/Solution.java
@@ -1,3 +1,19 @@
 class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
 
+        while (left <= right) {
+            if(people[right] + people[left] <= limit){
+                right--;
+                left++;
+            } else {
+                right--;
+            }
+            answer++;
+        }
+        return answer;
+    }
 }
