fa6e701f1bef7ecd19d46a952046298280fd7be9
zzkevin <realzzkevin@gmail.com>
2023-04-13 00:29:38 -0400

    solution for 71

--- 71_SimplifyPath/Solution.java
+++ 71_SimplifyPath/Solution.java
@@ -1,3 +1,26 @@
 class Solution {
+    public String simplityPath(String path) {
+        Stack<String> stack = new Stack<String>();
+        String[] subDir = path.split("/");
 
+        for(String dir : subDir) {
+            if(dir.isEmpty() || dir.equals(".")){
+                continue;
+            } else if(dir.equals("..")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            } else {
+                stack.add(dir);
+            }
+        }
+
+        StringBuilder newPath = new StringBuilder();
+        for(String dir : stack){
+            newpath.append("/");
+            newPath.append(dir);
+        }
+
+        return newpath.length() > 0 ? newPath.toString() : "/";
+    }
 }
