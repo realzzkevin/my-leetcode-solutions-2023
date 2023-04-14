171f253e8af03f3b9cd283db64adccb76a61436c
zzkevin <realzzkevin@gmail.com>
2023-04-13 23:19:17 -0400

    solution for 946

--- 946_ValidateStackSequences/Solution.java
+++ 946_ValidateStackSequences/Solution.java
@@ -1,3 +1,14 @@
 class Solution {
-
+    public boolean validateStackSequences(int[] pushed, int [] poped) {
+        Stack<Integer> stack = new stack<>();
+        int j = 0;
+        for(int i = 0; i < pushed.length; i++) {
+            stack.push(pushed[i]);
+            while(!stack.isEmpty() && stack.peek() == poped[j]){
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
 }
