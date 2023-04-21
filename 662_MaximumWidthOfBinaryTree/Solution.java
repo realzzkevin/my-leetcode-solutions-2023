337826a5825d0b86573628f2910c6157c838490d
zzkevin <realzzkevin@gmail.com>
2023-04-20 23:27:25 -0400

    solution for 662

--- 662_MaximumWidthOfBinaryTree/Solution.java
+++ 662_MaximumWidthOfBinaryTree/Solution.java
@@ -1,3 +1,30 @@
 class solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
 
+        LinkedList<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        Integer maxWitdth = 0;
+        queue.addLast(new Pair<>(root, 0));
+        while (queue.size() > 0){
+            Pair<TreeNode, Integer> head = queue.getFirst();
+            Integer currLevelSize = queue.size();
+            Pair<TreeNode, Integer> elem = null;
+            for(int i = 0; i < currLevelSize; ++i) {
+                elem = queue.removeFirst();
+                TreeNode node = elem.getKey();
+                if(node.left != null) {
+                    queue.addLast(new Pair<>(node.left, 2* elem.getValue()));
+                }
+                if(node.right != null){
+                    queue.addLast(new Pair<>(node.right, 2 * elem.getValue()) + 1);
+                }
+            }
+
+            maxWitdth = Math.max(maxWitdth, elem.getValue() - head.getValue() +1);
+        }
+
+        return maxWitdth;
+    }
 }
