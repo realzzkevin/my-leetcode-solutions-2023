569bb9a7bd8475504eef3eeefa431caba6bd0ff7
zzkevin <realzzkevin@gmail.com>
2023-04-25 23:36:17 -0400

    solution for 2336

--- 2336_SmallestNumberInIfiniteSet/Solution.java
+++ 2336_SmallestNumberInIfiniteSet/Solution.java
@@ -1,3 +1,30 @@
-class Solution {
+class SmallestInfiniteSet {
+    private SortedSet<Integer> addedIntegers;
+    private Integer currentInteger;
+
+
+    public SmallestInfiniteSet() {
+        addedIntegers = new TreeSet<>();
+        currentInteger = 1;
+    }
+
+    public int popSmallest() {
+        int answer;
+        if(!addedIntegers.isEmpty()){
+            answer = addedIntegers.first();
+            addedIntegers.remove(answer);
+        } else {
+            answer = currentInteger;
+            currentInteger++;
+        }
+        return answer;
+    }
+
+    public void addBack(int num) {
+        if(currentInteger <= num || addedIntegers.contains(num)){
+            return;
+        }
+        addedIntegers.add(num);
+    }
 
 }
