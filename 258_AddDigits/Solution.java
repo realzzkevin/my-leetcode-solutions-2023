66313ae5847464d4ba1d4d2a1a87706bacb0f9d0
zzkevin <realzzkevin@gmail.com>
2023-04-26 23:54:36 -0400

    solution for 258

--- 258_AddDigits/Solution.java
+++ 258_AddDigits/Solution.java
@@ -1,3 +1,13 @@
-class Soluton {
-
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10 ) {
+            int carry = 0;
+            while (num > 0) {
+                carry += num %10;
+                num /= 10;
+            }
+            num = carry;
+        }
+        return num;
+    }
 }
