9673e58ba216e88f52637c4d004387079c06bf19
zzkevin <realzzkevin@gmail.com>
2023-05-01 23:56:19 -0400

    solution for 1491

--- 1491_AverageSalaryExcMinAndMax/Solution.java
+++ 1491_AverageSalaryExcMinAndMax/Solution.java
@@ -1,3 +1,16 @@
 class Solution {
+    public double average(int[] salary) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int n : salary) {
+            sum += n;
+            min = Math.min(min, n);
+            max = Math.max(max, n);
+        }
 
+        sum -= min;
+        sum -= max;
+        return (double)sum/(salary.length-2);
+    }
 }
