ade08fa916caf8337c0c23300166a1417c3d2bee
zzkevin <realzzkevin@gmail.com>
2023-05-04 23:56:09 -0400

    solution for 1456

--- 1456_MaxNumberOfVowels/Solution.java
+++ 1456_MaxNumberOfVowels/Solution.java
@@ -1,3 +1,18 @@
 class Solution {
+    public int maxVowels(String s, int k) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < k; i++) {
+            alphabet[s.charAt(i) - 'a']++;
+        }
 
+        int maxVowels = alphabet[0] + alphabet[4] +alphabet[8] +alphabet[14] + alphabet[20];
+
+        for (int i = 0; i < s.length() - k; i++) {
+            alphabet[s.charAt(i) - 'a']--;
+            alphabet[s.charAt(i+k) -'a']++;
+            int temp = alphabet[0] + alphabet[4] +alphabet[8] +alphabet[14] + alphabet[20];
+            maxVowels = Math.max(maxVowels, temp);
+        }
+        return maxVowels;
+    }
 }
