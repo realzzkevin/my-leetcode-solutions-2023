631f2344e613f3558ded60b6a48574ca2c359562
zzkevin <realzzkevin@gmail.com>
2023-05-06 00:01:31 -0400

    solution for 1498

--- 1498_NumberOfSubsequences/Solution.java
+++ 1498_NumberOfSubsequences/Solution.java
@@ -1,3 +1,39 @@
 class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int n = nums.length;
+        int mod = 1000000007;
+        Arrays.sort(nums);
+
+        int[] power = new int[n];
+        power[0] = 1;
+        for(int i = 1; i < n; ++i){
+            power[i] = (power[i -1] * 2) % mod;
+        }
+
+        int answer = 0;
+        for(int left = 0; left < n; left++){
+            int right = biSearch(nums, target - nums[left]) -1;
+            if(right >= left) {
+                answer += power[right - left];
+                answer %= mod;
+            }
+        }
+
+        return answer;
+    }
+
+    public int biSearch(int[] nums, int target){
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
 
 }
