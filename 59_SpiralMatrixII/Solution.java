b8b8c76553f709393382a0a8ee3535e1e521f5e7
zzkevin <realzzkevin@gmail.com>
2023-05-10 23:34:02 -0400

    solution for 59

--- 59_SpiralMatrixII/Solution.java
+++ 59_SpiralMatrixII/Solution.java
@@ -1,3 +1,41 @@
 class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int size = 1;
+        int up = 0;
+        int left = 0;
+        int down = n-1;
+        int right = n-1;
 
+        while (size <= n*n){
+            for(int l = left; l <= right; l++){
+                matrix[up][l] = size;
+                size++;
+            }
+            for(int u = up+1; u <= down; u++){
+                matrix[u][right] = size;
+                size++;
+            }
+            if( right != left) {
+                for(int r = right-1; r >= left; r--){
+                    matrix[down][r] = size;
+                    size++;
+                }
+            }
+            if(up != down){
+                for(int d = down-1; d > up; d--){
+                    matrix[d][left] = size;
+                    size++;
+                }
+            }
+
+            up++;
+            left++;
+            down--;
+            right--;
+        }
+
+        return matrix;
+
+    }
 }
