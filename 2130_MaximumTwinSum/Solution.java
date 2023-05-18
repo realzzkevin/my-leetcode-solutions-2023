a4e463c2ef751cd293d61ef1614b3b0434ad2f0a
zzkevin <realzzkevin@gmail.com>
2023-05-17 23:59:16 -0400

    solution for 2130

--- 2130_MaximumTwinSum/Solution.java
+++ 2130_MaximumTwinSum/Solution.java
@@ -1,3 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
 
+        while( fast != null &&fast.next != null ) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode nextNode = null;
+        ListNode prev = null;
+
+        while( slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        ListNode start = head;
+        int max = 0;
+        while (prev != null) {
+            max = Math.max(max, start.val + prev.val);
+            prev = prev.next;
+            start = start.next;
+
+        }
+
+        return max;
+
+
+    }
 }
