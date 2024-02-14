a4cda2c9f5fe98959e5d5b46d14a5b1e0360a0be
zzkevin <realzzkevin@gmail.com>
2024-02-13 21:55:46 -0500

    solution for 2149

--- 368_LargestDivisibleSubset/Solution.java
+++ 368_LargestDivisibleSubset/Solution.java
@@ -36,4 +36,20 @@ class Solution {
 
         return subset;
     }
+}class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        int even = 0;
+        int odd = 1;
+        for ( int i = 0; i < nums.length; i++) {
+            if(nums[i]>=0){
+                ans[even] = nums[i];
+                even += 2;
+            } else {
+                ans[odd] = nums[i];
+                odd += 2;
+            }
+        }
+        return ans;
+    }
 }
